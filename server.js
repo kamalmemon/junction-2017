5b098edd02cd4d986384c3c88de152dee4f9feab
Naufal Khalid <naufalkhalid1991@kielikone.fi>
2017-11-25 15:17:23 +0200

    fix error

--- server.js
+++ server.js
@@ -44,14 +44,10 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res,next) {
-    if (err.code && err.code == "ETIMEDOUT")
-    {
-      err.status = 404
-    }
     res.status( err.status || 500 )
     .json({
       status: 'error',
-      message: err.message? err.message.message? err.message.message : err.message : err
+      message: err
     })
     .end();
   });
